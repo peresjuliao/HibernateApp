d2e240b553198565de27ac13c286853f39c61934
Peres Juliao <peresjuliao@gmail.com>
2012-03-29 15:52:06 -0300

    Atualizando...

--- src/br/com/hibernate/dao/GenericDaoImpl.java
+++ src/br/com/hibernate/dao/GenericDaoImpl.java
@@ -7,12 +7,13 @@ import org.hibernate.*;
 
 import br.com.hibernate.util.HibernateUtil;
 
-public abstract class GenericDaoImpl<T, ID extends Serializable> implements GenericDAO<T, Serializable> {
-	
+public abstract class GenericDaoImpl<T, ID extends Serializable> implements
+		GenericDAO<T, Serializable> {
+
 	private Session session;
 	private Transaction transaction;
 	private Class<T> persistentClass;
-	
+
 	public GenericDaoImpl(Class<T> persistentClass) {
 		this.persistentClass = persistentClass;
 	}
@@ -22,37 +23,37 @@ public abstract class GenericDaoImpl<T, ID extends Serializable> implements Gene
 		session = HibernateUtil.getSessionFactory().openSession();
 		transaction = session.beginTransaction();
 		session.save(object);
-		transaction.commit();          
+		transaction.commit();
 	}
-	
+
 	@Override
 	public void update(T object) {
-		 session = HibernateUtil.getSessionFactory().openSession();
-         transaction = session.beginTransaction();
-         session.update(object);
-         transaction.commit();		
+		session = HibernateUtil.getSessionFactory().openSession();
+		transaction = session.beginTransaction();
+		session.update(object);
+		transaction.commit();
 	}
-	
+
 	@Override
 	public void delete(T object) {
-		 session = HibernateUtil.getSessionFactory().openSession();
-         transaction = session.beginTransaction();
-         session.delete(object);
-         transaction.commit();		
+		session = HibernateUtil.getSessionFactory().openSession();
+		transaction = session.beginTransaction();
+		session.delete(object);
+		transaction.commit();
 	}
 
 	@Override
 	public T findById(Serializable id) {
 		session = HibernateUtil.getSessionFactory().openSession();
-		return  (T) session.load(persistentClass, id);				
+		return (T) session.load(persistentClass, id);
 	}
 
 	@Override
 	public List<T> findAll() {
 		List<T> resultList = null;
 		session = HibernateUtil.getSessionFactory().openSession();
-        resultList = session.createCriteria(persistentClass).list();
+		resultList = session.createCriteria(persistentClass).list();
 		return resultList;
-	}	
-	
+	}
+
 }
